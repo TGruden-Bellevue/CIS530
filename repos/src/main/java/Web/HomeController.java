8b28792ad27b71bd6fcee5473ecd9109646cff6f
Tricia <Tricia.Gruden@yahoo.com>
2025-03-29 19:38:21 -0400

    week 3

--- repos/src/main/java/Web/HomeController.java
+++ repos/src/main/java/Web/HomeController.java
@@ -1,7 +1,15 @@
 package Web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.impl.MemBookDao;
 
 @Controller
 public class HomeController {
@@ -20,4 +28,21 @@ public class HomeController {
     public String showContactUs() {
         return "contact";
     }
+
+    @GetMapping("/")
+    public String showHome(Model model) {
+        MemBookDao bookDao = new MemBookDao();
+        List<Book> books = bookDao.list();
+        model.addAttribute("books", books);
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String getMonthlyBook(@PathVariable String id, Model model) {
+        MemBookDao bookDao = new MemBookDao();
+        Book book = bookDao.find(id);
+        model.addAttribute("book", book);
+        return "monthly-books/view";
+    }
+
 }
